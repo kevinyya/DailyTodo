ed7ea086194fb669ae41c808756da113a9a4836f
kevinyya <yyakevin@outlook.com>
2024-12-06 22:36:13 +0800

    Add MaterialCalendarView

--- app/src/main/java/com/example/dailytodo/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/dailytodo/ui/calendar/CalendarFragment.kt
@@ -1,13 +1,16 @@
 package com.example.dailytodo.ui.calendar
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.dailytodo.databinding.FragmentCalendarBinding
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import java.util.Calendar
+
 
 class CalendarFragment : Fragment() {
 
@@ -28,10 +31,16 @@ class CalendarFragment : Fragment() {
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCalendar
-        calendarViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        // val textView: TextView = binding.textCalendar
+        // calendarViewModel.text.observe(viewLifecycleOwner) {
+        //     textView.text = it
+        // }
+
+        // Highlight Current Date
+        val currentDate: Calendar = Calendar.getInstance()
+        binding.calendarView.setSelectedDate(currentDate.getTime())
+
+
         return root
     }
 
